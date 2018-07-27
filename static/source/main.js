1fcd41ffc966ba4b1f164bef25d875f0d5828d79
xinghalo <xinghalo@163.com>
2018-07-27 23:06:50 +0800

    修改图形移动绘制效果

--- static/source/main.js
+++ static/source/main.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
+
   // 鼠标按下的位置
-  var x_down = -1;
-  var y_down = -1;
   var draw_rec = false;
 
   $('#img').bind("mousedown","div",function(event){
@@ -14,42 +13,71 @@ $(document).ready(function(){
         x_down = event.originalEvent.x || event.originalEvent.layerX || 0;
         y_down = event.originalEvent.y || event.originalEvent.layerY || 0;
 
+        var img_top = target.offset().top;
+        var img_left = target.offset().left;
+
+        // 创建新的div，宽度高度固定为0
+        var rec_x = x_down - img_left;
+        var rec_y = y_down - img_top;
+
+        var new_div = "<span style='position:absolute;border-style:solid;border-color:red;"
+            + "top:"    + rec_y + "px;"
+            + "left:"   + rec_x + "px;"
+            + "width:0px;height:0px;'></span>";
+        $('#img').append(new_div);
         draw_rec = true;
+
         console.log("鼠标按下的位置:"+x_down+","+y_down);
+    }
+  });
+
+  $('#img').mousemove(function(event){
+    var x_mouse = event.pageX || 0; // 鼠标x
+    var y_mouse = event.pageY || 0; // 鼠标y
+
+    if(draw_rec){
+        var _current = $('#img').children().last();
+        var rec_top  = _current.offset().top;   // y
+        var rec_left = _current.offset().left;  // x
+
+        // 假设y比top大
+        _current.css("width",(x_mouse - rec_left)+"px");
+        _current.css("height",(y_mouse - rec_top)+"px");
     }
   });
 
   $('#img').bind("mouseup","div",function(event){
     var target = $(event.target);
     if(target.prop("nodeName")=="DIV" && draw_rec){
+        draw_rec = false;
         // 获得鼠标抬起的坐标信息
-        var xx = event.originalEvent.x || event.originalEvent.layerX || 0;
-        var yy = event.originalEvent.y || event.originalEvent.layerY || 0;
-
-        // 获得对象的坐标信息
-        var img_top = target.offset().top;
-        var img_left = target.offset().left;
-
-        console.log("鼠标抬起的位置:"+xx+","+yy);
-        console.log("图片的位置:"+img_left+","+img_top)
-
-        // 创建新的div
-        var rec_x = x_down - img_left;
-        var rec_y = y_down - img_top;
-        var rec_w = xx - x_down;
-        var rec_h = yy - y_down;
-
-        var new_div = "<span style='position:absolute;border-style:solid;border-color:red;"
-            + "top:"    + rec_y + "px;"
-            + "left:"   + rec_x + "px;"
-            + "width:"  + rec_w + "px;"
-            + "height:" + rec_h + "px;"
-            + "'></span>";
-
-        $('#img').append(new_div);
-
-        x_down = -1;
-        y_down = -1;
+//        var xx = event.originalEvent.x || event.originalEvent.layerX || 0;
+//        var yy = event.originalEvent.y || event.originalEvent.layerY || 0;
+//
+//        // 获得对象的坐标信息
+//        var img_top = target.offset().top;
+//        var img_left = target.offset().left;
+//
+//        console.log("鼠标抬起的位置:"+xx+","+yy);
+//        console.log("图片的位置:"+img_left+","+img_top)
+//
+//        // 创建新的div
+//        var rec_x = x_down - img_left;
+//        var rec_y = y_down - img_top;
+//        var rec_w = xx - x_down;
+//        var rec_h = yy - y_down;
+//
+//        var new_div = "<span style='position:absolute;border-style:solid;border-color:red;"
+//            + "top:"    + rec_y + "px;"
+//            + "left:"   + rec_x + "px;"
+//            + "width:"  + rec_w + "px;"
+//            + "height:" + rec_h + "px;"
+//            + "'></span>";
+//
+//        $('#img').append(new_div);
+//
+//        x_down = -1;
+//        y_down = -1;
     }
   });
 
